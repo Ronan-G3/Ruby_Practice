1f7527e62e45f9f1d53e5e872bd42d85f5a0d73f
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-10-31 17:03:45 +0000

    Ruby Week 6 Practice files

--- W6 practice/inherit.rb
+++ W6 practice/inherit.rb
@@ -28,5 +28,20 @@ class Seagull < Bird # I want to inherit all methods that belong to bird into se
   def beak_size
     @beak + @beak
   end
+  class Kiwi < Bird
+    attr_accessor :cute
 
+    def initialize(name, wings, legs, beak)
+      @name = name
+      @wings = false
+      @legs = legs
+      @beak = beak
+      @flies = false
+      @feathers = true
+      @cute = true
+    end
+    def opens_shoe_polish_tins?
+      instance_of?(Kiwi)
+    end
+  end
 end
